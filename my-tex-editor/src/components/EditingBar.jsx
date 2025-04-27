60c4a9a12e28369d809bd5d769b01b884904b877
MounikaRadha <radha.mounika@testsigma.com>
2025-04-27 12:41:34 +0530

    feat/adding quotes,pre

--- my-tex-editor/src/components/EditingBar.jsx
+++ my-tex-editor/src/components/EditingBar.jsx
@@ -1,23 +1,24 @@
 export default function Editingbar() {
-  const formatText = (command,value=null) => {
-    document.execCommand(command,false, value);
+  const formatText = (command, value = null) => {
+    document.execCommand(command, false, value);
+  };
+  const insertHtml = (tag) => {
+    document.execCommand(
+      "insertHTML",
+      false,
+      `<${tag}>${document.getSelection().toString()}</${tag}>`
+    );
   };
   return (
     <div className="w-screen flex justify-center gap-4 ">
       <span>editing bar </span>
       &nbsp;
-      <button onClick={() => formatText("bold")}>
-        Bold
-      </button>
-      <button onClick={() => formatText("italic")} >
-        Italic
-      </button>
-      <button onClick={() => formatText("underline")}>
-        Underline
-      </button>
-      <button  onClick={()=>formatText("formatBlock","<h1>")}>
-        heading
-      </button>
+      <button onClick={() => formatText("bold")}>Bold</button>
+      <button onClick={() => formatText("italic")}>Italic</button>
+      <button onClick={() => formatText("underline")}>Underline</button>
+      <button onClick={() => formatText("formatBlock", "<h1>")}>heading</button>
+      <button onClick={() => insertHtml("q")}>quotes</button>
+      <button onClick={() => insertHtml("pre")}>code block</button>
     </div>
   );
 }
